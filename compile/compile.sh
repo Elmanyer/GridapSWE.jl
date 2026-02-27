e6e6a711f42cfb3713a39e5b243bf32f6cf1097c
Pau Manyer Fuertes <pmanyerfuertes@tudelft.nl>
2026-02-27 14:15:54 +0100

    Corrected compile.sh bash job file to execute in memory node. #master

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name="compile_GridapSWE"
-#SBATCH --partition=compute
-#SBATCH --time=04:00:00
+#SBATCH --partition=memory
+#SBATCH --time=16:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=3G
+#SBATCH --mem-per-cpu=16G
 #SBATCH --output=GridapSWE.%j.out
 #SBATCH --error=GridapSWE.%j.err
 
