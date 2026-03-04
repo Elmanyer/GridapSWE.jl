e3f0d682c6e5b133db7c782ee805db33b2a3674a
Pau Manyer Fuertes <pmanyerfuertes@tudelft.nl>
2026-03-04 17:59:58 +0100

    Correct set_preferences specific packages' branches #master

--- compile/set_preferences.jl
+++ compile/set_preferences.jl
@@ -5,9 +5,9 @@ using Pkg
 MPIPreferences.use_system_binary()
 
 # Add specific branches of Gridap and GridapDistributed and GridapSolvers
-Pkg.add("Gridap#distributed_AD_transient")
-Pkg.add("GridapDistributed#distributed_AD_transient")
-Pkg.add("https://github.com/Elmanyer/GridapSolvers.jl.git#fix_num_fields")
+Pkg.add(PackageSpec(name="Gridap", rev="distributed_AD_transient"))
+Pkg.add(PackageSpec(name="GridapDistributed", rev="distributed_AD_transient"))
+Pkg.add(PackageSpec(url="https://github.com/Elmanyer/GridapSolvers.jl.git", rev="fix_num_fields"))
 
 # Execution in terminal
 # julia --project=. set_preferences.jl
