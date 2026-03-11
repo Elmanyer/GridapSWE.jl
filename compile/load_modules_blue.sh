703d738df9f2d203bba62fd5917c56e56ff2a7a0
Pau Manyer Fuertes <pmanyerfuertes@tudelft.nl>
2026-03-11 09:32:28 +0100

    add Preferences package; fix load_modules_blue mpi_path #master

--- compile/load_modules_blue.sh
+++ compile/load_modules_blue.sh
@@ -5,6 +5,6 @@ export MPI_ROOT=$(dirname $(mpicc --showme:libdirs | awk '{print $1}'))
 export MPI_LIB_DIR="$MPI_ROOT/lib"
 export LD_LIBRARY_PATH=$MPI_LIB_DIR:$LD_LIBRARY_PATH
 
-export LD_PRELOAD="$MPI_PATH/libmpi.so"
+export LD_PRELOAD="$MPI_LIB_DIR/libmpi.so"
 export JULIA_MPI_BINARY=system
 export JULIA_MPI_PATH=$MPI_ROOT
